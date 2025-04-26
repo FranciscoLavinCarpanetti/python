272a5e1b3f3ef002ef3001410c2cf21b39420d59
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-04-26 18:59:30 +0200

    feat(type-conversion): añadir ejemplos de conversión de tipos y redondeo en Python
    Se añadieron ejemplos prácticos de cómo convertir entre diferentes tipos de datos (str, int, float, bool) y cómo utilizar la función round() para redondear números en Python.

---
+++ 03_cast.py
@@ -0,0 +1,13 @@
+print("conversion de tipos")
+print(type(int("100")))
+print(type(float("100.5")))
+print(int(100.5))
+
+print(bool("100"))
+print(bool(-100))
+print(bool(0.0))
+
+print (int(2.5))
+
+print(round(2.5)) #redondea al número par más cercano, .5 siempre redondea hacia abajo
+print(round(2.6))
