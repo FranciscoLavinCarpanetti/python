39f7e90449d44926314d83b78017e454fe2fa6ee
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-04-26 18:26:24 +0200

    chore(python): iniciar proyecto y primeras pruebas con print

---
+++ 01_basic/01_print.py
@@ -0,0 +1,6 @@
+
+print("¡Hola Mundo soy Francisco Lavin aprendiendo a programar en Python!")
+
+print("python", "es", "un", "lenguaje", "de", "programación", sep="-")
+
+print("python", "es", "un", "lenguaje", "de", "programación", sep="-", end=".\n")
