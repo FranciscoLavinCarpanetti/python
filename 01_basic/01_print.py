23b47823a51aafa1a6563018afb4caa55f3177c8
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-04-26 18:43:25 +0200

    feat(types): añadir ejemplos de tipos de datos básicos en Python
    Se añadieron variables de tipo int, float, str y bool, para practicar y entender los diferentes tipos de datos que maneja Python.

--- 01_basic/01_print.py
+++ 01_basic/01_print.py
@@ -4,3 +4,5 @@ print("¡Hola Mundo soy Francisco Lavin aprendiendo a programar en Python!")
 print("python", "es", "un", "lenguaje", "de", "programación", sep="-")
 
 print("python", "es", "un", "lenguaje", "de", "programación", sep="-", end=".\n")
+
+print("36")
---
+++ 02_types.py
@@ -0,0 +1,29 @@
+print("int:")
+print(type(5))
+print (type(55))
+
+print("float:")
+print(type(5.0))
+print(type(5.5))
+
+print("complex:")
+print(type(5j))
+print(type(5.0j))
+
+print("str:")
+print(type("hola"))
+print(type('hola'))
+print(type('''hola'''))
+
+print("bool:")
+print(type(True))
+print(type(False))
+print(type(5 == 5))
+print(type(5 != 5))
+print(type(6 > 5))
+
+print("nonetype:")
+print(type(None))
+
+
+
