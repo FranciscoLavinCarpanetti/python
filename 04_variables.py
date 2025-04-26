0512921d82e7e026486a9a96bbd69f225e5988e4
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-04-26 19:42:01 +0200

    feat(variables): añadir ejemplos de variables, constantes y tipos anotados en Python
    Se añadieron ejemplos de declaración de variables, asignación múltiple, uso de f-strings para impresión en consola y definición de constantes siguiendo las convenciones de Python.
    También se incluyeron anotaciones de tipo para variables booleanas y de texto.

---
+++ 04_variables.py
@@ -0,0 +1,30 @@
+my_name="francisco javier"
+my_age=36
+my_height=1.78
+my_weight=145   
+
+print(f"my name is {my_name}")
+print(f"my age is {my_age}")
+print(f"my height is {my_height}")
+print(f"my weight is {my_weight}")
+print(f"my name is {my_name} and my age is {my_age}")
+print(f"my name is {my_name} and my height is {my_height}")
+
+name, age, height, weight = "francisco javier", 36, 1.78, 145
+print(f"my name is {name}")
+print(f"my age is {age}")
+print(f"my height is {height}")
+print(f"my weight is {weight}")
+print(f"my name is {name} and my age is {age}")
+print(f"my name is {name} and my height is {height}")
+
+MI_CONSTANTE = 3.1416 #No se debe cambiar el valor de una constante (por convencion se escriben en mayusculas)
+
+
+# Variables de tipo
+
+is_user_logged_in: bool = True
+user_name: str = "Francisco Javier"
+print(f"Is user logged in? {is_user_logged_in}")
+
+
