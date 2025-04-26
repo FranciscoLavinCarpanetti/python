8489579d78389b5812b703a8418c9e902aee314a
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-04-26 21:14:15 +0200

    feat(conditionals): añadir ejemplos de uso de if, elif, else y operadores lógicos en Python
    Se añadieron ejemplos de control de flujo utilizando estructuras condicionales básicas y múltiples condiciones con and, or y not.
    También se practicó el anidado de condiciones y buenas prácticas para mantener el código claro y legible.

---
+++ 02_flow_control/01_if.py
@@ -0,0 +1,74 @@
+###
+# sentencia condicional (if, elif, else)
+# permiten ejecutar un bloque de código si se cumple una condición
+###
+
+import os
+os.system("clear") # import os y limpiar la consola
+
+edad = 18
+if edad >= 18:
+ print("Eres mayor de edad") # se ejecuta si la condición es verdadera dentro del bloque if ()
+
+
+ edad = 17
+if edad >= 18:
+ print("Eres mayor de edad") # no se ejecuta si la condición es falsa dentro del bloque if ()
+else:
+ print("Eres menor de edad") # se ejecuta si la condición es falsa dentro del bloque else ()
+
+# se puede usar elif para agregar más condiciones
+
+nota = 7
+if nota >= 9:
+ print("Excelente")
+elif nota >= 7:
+ print("Buena")
+elif nota >= 5:
+ print("Regular")
+elif nota >= 3:
+ print("Mala")  
+else:
+    print("¡no estas calificado!") # se ejecuta si la condición es falsa dentro del bloque else ()
+
+
+# Condiciones multiples
+# se pueden usar operadores logicos para combinar condiciones
+# and: ambas condiciones deben ser verdaderas
+# or: al menos una de las condiciones debe ser verdadera
+# not: invierte el valor de la condición
+edad = 18
+if edad >= 18 and edad < 65:
+ print("Eres adulto")
+elif edad >= 65:
+ print("Eres adulto mayor")
+else:
+ print("Eres menor de edad")
+
+carnet_conducir = True
+
+if edad >= 18 or carnet_conducir:
+    print("Puedes conducir")
+else:
+    print("No puedes conducir")
+
+
+es_fin_de_semana = True
+if not es_fin_de_semana:
+    print("Puedes descansar")   
+
+edad =18
+tiene_dinero = True
+if edad >= 14:
+  if tiene_dinero:
+      print("Puedes comprar alcohol")
+  else:
+        print("No puedes comprar alcohol")
+else:
+    print("No puedes comprar alcohol")   
+
+#cuidado con el anidado de condiciones
+# se recomienda usar funciones para evitar el anidado de condiciones
+
+
+
