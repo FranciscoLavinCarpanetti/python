7fdf544edb4ef496dbee144ab25e8d0be4f56261
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-04-30 16:19:56 +0200

    feat: implementa alternativa optimizada con diccionario para encontrar la primera suma que cumpla el objetivo
    - Se utiliza un diccionario para almacenar los valores ya vistos y sus índices.
    - Mejora el rendimiento reduciendo la complejidad de O(n²) a O(n).
    - Devuelve los índices de los dos números cuya suma coincide con el objetivo.

--- 04_logic/03_challenge_find_first_sum.py
+++ 04_logic/03_challenge_find_first_sum.py
@@ -12,3 +12,19 @@ nums = [1, 2, 6, 4, 2]
 goal = 8
 result = find_first_sum(nums, goal)
 print(result)  # Output the result
+
+
+
+# Solución alternativa con diccionarios
+def find_first_sum(nums, goal):
+    seen = {} # disccionario para almacenar numeros e indices
+    for index, value in enumerate(nums):
+        missing = goal - value
+        if missing in seen: return [seen [missing], index]
+        seen[value] = index
+    return None  # Return None if no pair is found
+        
+nums = [1, 2, 6, 4, 2]
+goal = 8
+result = find_first_sum(nums, goal)
+print(result)  # Output the result
